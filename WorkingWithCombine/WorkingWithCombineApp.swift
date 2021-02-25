831430c8d8b1b497ec1fc97d41d7c6d0b10e9874
Josh Kinney <joshkinney@kinneykare.com>
2021-02-24 19:45:04 -0600

    Initial Commit

---
+++ WorkingWithCombine/WorkingWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithCombineApp.swift
+//  WorkingWithCombine
+//
+//  Created by Kinney Kare on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
